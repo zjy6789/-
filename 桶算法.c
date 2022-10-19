8d4ce7891b5c316ed909414088ad7d7968ad4a4d
zjy6789 <112699896+zjy6789@users.noreply.github.com>
2022-10-19 20:32:01 +0800

    Add files via upload
    简化版桶算法

---
+++ 桶算法.c
@@ -0,0 +1,21 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+int main()
+{
+	int a[101];
+	int n, i, m=0, j;
+	for (i=0; i <= 100;  i++)
+		a[i] = 0;
+	scanf("%d", &n);
+	for (i=1; i <= n; i++)
+	{
+		scanf("%d", &m);
+		a[m]++;
+	}
+	for (i=100; i >=0; i--)
+	{
+		for (j = 1;j <= a[i];j++)
+			printf("%d  ", i);
+	}
+	return 0;
+}

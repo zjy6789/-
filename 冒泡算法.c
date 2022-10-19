d80222f7bbe67ea53572da85f6da9ba7679fab05
zjy6789 <112699896+zjy6789@users.noreply.github.com>
2022-10-19 20:30:16 +0800

    Add files via upload

---
+++ 冒泡算法.c
@@ -0,0 +1,23 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+int main()
+{
+	int a[100];
+	int n, i, m, j, q;
+	scanf("%d", &n);
+	for (i = 1; i <= n; i++)
+		scanf("%d", &a[i]);
+	for (i = 1;i <= n - 1;i++)
+	{
+		for (m=1;m <= n - 1;m++)
+		{
+			if (a[m] < a[m+1])
+			{
+				j = a[m];a[m] = a[m+1];a[m+1] = j;
+			}
+		}
+	}
+	for (i = 1;i <= n;i++)
+		printf("%d  ", a[i]);
+	return 0;
+}
